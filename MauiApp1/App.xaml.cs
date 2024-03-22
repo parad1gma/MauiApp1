0894d3656bcbb75f62728d8522571988d80effb9
Mile Kordic <mile.kordic@elanwave.com>
2024-03-22 11:31:46 +0100

    Add project files.

---
+++ MauiApp1/App.xaml.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+using Microsoft.AppCenter.Distribute;
+
+namespace MauiApp1
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+
+        bool OnReleaseAvailable(ReleaseDetails releaseDetails)
+        {
+            AppCenterLog.Info("MauiApp1", "OnReleaseAvailable id=" + releaseDetails.Id
+                                            + " version=" + releaseDetails.Version
+                                            + " releaseNotesUrl=" + releaseDetails.ReleaseNotesUrl);
+            var custom = releaseDetails.ReleaseNotes?.ToLowerInvariant().Contains("custom") ?? false;
+            if (custom)
+            {
+                var title = "Version " + releaseDetails.ShortVersion + " available!";
+                Task answer;
+                if (releaseDetails.MandatoryUpdate)
+                {
+                    answer = Current.MainPage.DisplayAlert(title, releaseDetails.ReleaseNotes, "Update now!");
+                }
+                else
+                {
+                    answer = Current.MainPage.DisplayAlert(title, releaseDetails.ReleaseNotes, "Update now!", "Maybe tomorrow...");
+                }
+                answer.ContinueWith((task) =>
+                {
+                    if (releaseDetails.MandatoryUpdate || ((Task<bool>)task).Result)
+                    {
+                        Distribute.NotifyUpdateAction(UpdateAction.Update);
+                    }
+                    else
+                    {
+                        Distribute.NotifyUpdateAction(UpdateAction.Postpone);
+                    }
+                });
+            }
+            return custom;
+        }
+
+        void OnNoReleaseAvailable()
+        {
+            AppCenterLog.Info("MauiApp1", "No release available callback invoked.");
+        }
+
+        protected override void OnStart()
+        {
+            base.OnStart();
+
+            Distribute.SetEnabledForDebuggableBuild(true);
+
+            Distribute.ReleaseAvailable = OnReleaseAvailable;
+            Distribute.NoReleaseAvailable = OnNoReleaseAvailable;
+
+            // Initialize App Center SDK
+            AppCenter.Start("android=ca055c3e-25d8-4d6b-93a1-1e411558b73c", typeof(Distribute), typeof(Analytics), typeof(Crashes));
+
+        }
+    }
+}
